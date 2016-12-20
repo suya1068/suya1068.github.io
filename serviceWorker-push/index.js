7e25b2c445ed519de227000006849b588c8fc472
sungsoo kim <suya1068@gmail.com>
2016-12-20 10:57:17 +0900

    commit-2 promis 수정

--- serviceWorker-push/index.js
+++ serviceWorker-push/index.js
@@ -1,6 +1,7 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-// Registration was successful
+    navigator.serviceWorker.register('service-worker.js')
+        .then(function(registration) {
+        // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
         registration.pushManager.subscribe({userVisibleOnly: true})
             .then(function(subscription){
@@ -8,6 +9,8 @@ if ('serviceWorker' in navigator) {
             console.log("subscription.subscriptionId: ", subscription.subscriptionId);
             console.log("subscription.endpoint: ", subscription.endpoint);
             return sendSubscriptionToServer(subscription);
+        }).catch(function(e) {
+            console.log("서버에 보내기 실패했엉", e);
         })
     }).catch(function(err) {
 // registration failed :(
