59441c7d0371b31c7e42fe3a9b7c5d162af62faf
sskim <suyakim@forsnap.com>
2018-02-19 11:37:51 +0900

    sskim 코드 수정.

--- init.js
+++ init.js
@@ -20,23 +20,25 @@ if ('serviceWorker' in navigator) {
             // registration failed :(
             console.log('ServiceWorker registration failed: ', err);
         });
-        var CACHE_NAME = 'my-site-cache-v1';
-        var urlsToCache = [
-            '/',
-            '/image'
-        ];
-
-        self.addEventListener('install', function(event) {
-            // Perform install steps
-            event.waitUntil(
-                caches.open(CACHE_NAME)
-                    .then(function(cache) {
-                        console.log('Opened cache');
-                        return cache.addAll(urlsToCache);
-                    })
-            );
-        });
     });
+    
+    var CACHE_NAME = 'my-site-cache-v1';
+    var urlsToCache = [
+        '/',
+        '/image'
+    ];
+
+    self.addEventListener('install', function(event) {
+        // Perform install steps
+        event.waitUntil(
+            caches.open(CACHE_NAME)
+                .then(function(cache) {
+                    console.log('Opened cache');
+                    return cache.addAll(urlsToCache);
+                })
+        );
+    });
+
     self.addEventListener('fetch', function(event) {
         event.respondWith(
             caches.match(event.request)
