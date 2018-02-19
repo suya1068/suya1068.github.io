5c0c71c3954652c9459174ff38d6e45e7bebd2eb
sskim <suyakim@forsnap.com>
2018-02-19 12:23:34 +0900

    sskim 홈화면 설치 프롬프트 테스트

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -39,7 +39,7 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function(cache) {
-                console.log('Opened cache');
+                console.log('Opened cache', cache);
                 return cache.addAll(urlsToCache);
             }).catch(function (error) {
             console.log("cache faile", error);
@@ -60,4 +60,40 @@ self.addEventListener('fetch', function(event) {
                 }
             )
     );
+});
+
+var deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', function(e) {
+    console.log('beforeinstallprompt Event fired');
+    e.preventDefault();
+
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+
+    return false;
+});
+
+btnSave.addEventListener('click', function() {
+    if(deferredPrompt !== undefined) {
+        // The user has had a postive interaction with our app and Chrome
+        // has tried to prompt previously, so let's show the prompt.
+        deferredPrompt.prompt();
+
+        // Follow what the user has done with the prompt.
+        deferredPrompt.userChoice.then(function(choiceResult) {
+
+            console.log(choiceResult.outcome);
+
+            if(choiceResult.outcome === 'dismissed') {
+                console.log('User cancelled home screen install');
+            }
+            else {
+                console.log('User added to home screen');
+            }
+
+            // We no longer need the prompt.  Clear it up.
+            deferredPrompt = null;
+        });
+    }
 });
